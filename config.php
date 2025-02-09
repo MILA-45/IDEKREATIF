8970e74e9f287445dc86504f94384a6f8a8d817d
MILA-45 <mila.mustika45@smk.belajar.id>
2025-02-09 20:08:43 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// konfigurasi koneksi database
+$host = "localhost"; // nama host server database
+$username = "root"; // username untuk akses ke database
+$password = ""; // password untuk akses ke database
+$database = "IDE_KREATIF"; // nama database yang digunakan
+
+// membuat koneksi ke database menggunakan database mysqli
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// mengecek apakah koneksi berhasil
+if (!$conn) {
+    // menampilkan pesan error jika koneksi gagal
+    die("Database gagal terkoneksi: " . mysqli_connect_error());
+}
+
+// jika koneksi berhasil script akan jalan tanpa pesan error
+?>
